3d137fd75ab130f68fd9f75fef74efdeab961733
DeGondim <60111866+DeGondim@users.noreply.github.com>
2021-08-23 20:49:44 -0300

    Add files via upload

---
+++ pelson/js/script.js
@@ -0,0 +1,3 @@
+function funcao() {
+document.getElementById("msgmodal").innerHTML = "Desculpe, você não tem a idade necessária para acessar o conteúdo.\n Conteúdo permitido apenas para maiores de 18 anos!";
+}
