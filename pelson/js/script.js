e159c1cd3d43f8c9e57bc75c41725ed51a127d9d
DeGondim <60111866+DeGondim@users.noreply.github.com>
2021-08-23 20:51:51 -0300

    Delete pelson/js directory

--- pelson/js/script.js
+++
@@ -1,3 +0,0 @@
-function funcao() {
-document.getElementById("msgmodal").innerHTML = "Desculpe, você não tem a idade necessária para acessar o conteúdo.\n Conteúdo permitido apenas para maiores de 18 anos!";
-}
